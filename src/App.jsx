1bb3e372ed54e74febc762a7c7042b2e805f9c9b
Danilogomez5 <danilog992@gmail.com>
2023-09-25 18:06:46 -0300

    Danilo(?

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Laura from "./Components/Laura";
 import Alexis from "./Components/Alexis";
 import Jesi from "./Components/Jesi";
 import Nico from "./Components/Nico"
+import Danilo from "./Components/Danilo";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
       <Alexis/>
       <Nico/>
       <Jesi />
-
+      <Danilo/>
     </>
   );
 }
